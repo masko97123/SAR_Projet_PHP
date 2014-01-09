1874f57ef9de7a98f4fb4a9b79c168a80a68259a
masko97123 <d.pochal971@gmail.com>
2014-01-09 09:54:53 +0100

    Initial commit

---
+++ Site/index.php
@@ -0,0 +1,239 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="fr"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8 is-ie-7" lang="fr"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9 is-ie8" lang="fr"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="fr" dir="ltr" id="html"><!--<![endif]-->
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="author" content="Pochal david"/>
+    <meta name="description" content="Page d'accueil SAR Environnement"/>
+    <meta name="keywords" content="Recyclace, meteaux, ferreux, non-ferreux,"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/reset.css" type="text/css"/>
+    <link rel="stylesheet" href="css/normalize.css" type="text/css"/>
+    <link rel="stylesheet" href="css/styles.css" type="text/css"/>
+    <link rel="stylesheet" href="css/flexslider.css" type="text/css"/>
+
+    <script src="js/jquery-1.8.3.min.js"></script>
+    <script>
+            document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/,'js');
+        </script>
+
+        <!--[if lt IE 9]>
+            <script src="dist/html5shiv.js"></script>
+        <![endif]-->
+        <script type="text/javascript" src="js/modernizr.custom.28468.js">
+        </script>
+        <noscript>
+                <link rel="stylesheet" type="text/css" href="css/nojs.css" />
+        </noscript>
+
+
+    <title>SAR Environnement</title>
+
+</head>
+<body role="document" id="accueil">
+     <header role="banner" id="nav">
+        <div class="wrapper">
+            <a href="#" rel="home">
+                <h1 class="hide-text">Sar Entreprise</h1>
+            </a>
+             <nav role="navigation">
+                 <ul>
+                     <li><a href="#accueil" title="Accueil" class="is-active">Accueil</a></li>
+                     <li><a href="#so-scroller" title="so-scroller">Entreprise</a></li>
+                     <li><a href="#activites" title="activites">Activités</a></li>
+                     <li><a href="#clients" title="clients">Clients</a></li>
+                     <li><a href="#contacts" title="contacts">Contacts</a></li>
+                 </ul>
+             </nav>
+         </div>
+     </header><!-- FIN DU HEADER -->
+
+     <section role="main" class="clearfix">
+         <div class="wrapper">
+             <img src="img/index-img.png" alt="#" id="index-img" title="#">
+             <p><a href="#" class="inner-link"><b>SAR environnement</b></a>, adipiscing et, sodales ac, feugiat non, lacus. Ut dictum velit nec est. Quisque posuere, purus sit amet malesuada blandit, sapien sapien auctor arcu, sed pulvinar felis mi sollicitudin tortor. Maecenas volutpat, nisl et dignissim pharetra, urna lectus ultrices est, vel pretium pede turpis id velit. Aliquam sagittis magna in felis egestas</p>
+             <a href="" class="btn btn-3 btn-3e icon-arrow-right" title="#">Demandez votre devis</a>
+             <div class="scroll-btn floating"></div>
+         </div>
+     </section><!-- FIN DE INDEX -->
+
+     <section id="so-scroller" class="so-scroller clearfix">
+         <div class="wrapper so-section clearfix">
+             <header class="so-side so-side-left"><h2 class="big-title">l’entreprise</h2></header>
+             <article role="article" class="art-1 clearfix so-side so-side-right">
+                 <img src="img/entreprise-img-1.png" alt="Les différents logo de l'entreprise depuis 1952" title="Ancien logo">
+                 <h3 class="small-title">Depuis 1952</h3>
+                 <p>Depuis plus d’un demi-siècle, elle propose aux entreprises et industries <a href="#"><b>des solutions de gestion, de valorisation et traitement</b></a> de tous types de déchets.
+                 La prestation de services est assurée dans le cadre de démontage et de démolition de remontées mécaniques et de site industriel en partie ou en totalité.
+                 Au fil des décennies, l’entreprise a mis en place un éventail de procédés pour gérer les déchets générés, dans un esprit de qualité.</p>
+             </article>
+
+             <article role="article" class="art-2 clearfix so-side so-side-left">
+                <img src="img/entreprise-img-2.png" alt="Bras portant un casque de chantier" title="Casque de chantier porter au bras">
+                 <h3 class="small-title">Nos moyens à votre service</h3>
+                 <p>Une équipe de 10 personnes dynamiques, réactives, formées et expérimentées. <a href="#"><b>2 sites (Ste Hélène sur Isère 1 HT et Tournon 2 HT)</b></a> <abbr title="#">CPE</abbr> soumise à autorisation et déclaration. Récépissé pour l’activité de transport par route de déchets. Récépissé pour l’activité de négoce ou de courtage de déchets. Plateforme de tri et de transit dans le but de valoriser les déchets.</p>
+
+             </article>
+         </div>
+     </section><!-- FIN ENTREPRISE -->
+
+    <blockquote class="quote-sec">
+        <p>60 ans d’expériences acquises dans leur domaine
+        font de la S.A.R Environnement un prestataire compétent.
+        <span>
+            <b>André Frémon,</b>
+            <i> chef de chantier.</i>
+        </span>
+        </p>
+    </blockquote>
+    <div id="activites">
+        <h2>Nos activités</h2>
+      <section class="slider">
+        <div class="flexslider">
+          <ul class="slides">
+            <li class="clearfix">
+                <h3 class="small-title">Gestion des déchets</h3>
+                <div class="art-3">
+                    <span class="art-picto"></span>
+                    <p>Nous livrons <a href="#">les bennes commandées</a> selon deux critères majeurs :<b>Les normes de sécurité & La place sur le chantier</b></p>
+                </div>
+                <div class="art-4">
+                    <span class="art-picto"></span>
+                    <p>Un tri en amont est effectué sur site à la récupération des bennes (dans la mesure du possible) pour une meilleure valorisation des déchets. </p>
+                </div>
+                <div class="art-5">
+                    <span class="art-picto"></span>
+                    <p>Dès leur retour dans nos centres spécialisés, les déchets non triés le sont et les autre subissent une valorisation matière ou énergétique.</p>
+                </div>
+                <div class="art-6 clearfix">
+                    <span class="art-picto"></span>
+                    <p>La part valorisable est extraite et le déchet ultime (non valorisable) part en centre de classe 2  pour enfouissement.</p>
+                </div>
+            </li><!-- FIN SLIDE #1-->
+            <li class="clearfix">
+                <h3 class="small-title">Location de bennes </h3>
+                <div class="art-7">
+                    <span class="art-picto"></span>
+                    <p>Nous mettons à la disposition  des professionnels du secteur privé , du secteur public  et des particuliers.</p>
+                </div>
+                <div class="art-8">
+                    <span class="art-picto"></span>
+                    <p>Nous vous proposons une offre personalisé grace à notre parc de <b>300 bennes</b> allant de <b>5m<sup>3</sup> à 35m<sup>3</sup>.</b> </p>
+                </div>
+                <div class="art-9">
+                    <span class="art-picto"></span>
+                    <p>Dès leur retour dans nos centres spécialisés, les déchets non triés le sont et les autre subissent une valorisation matière ou énergétique.</p>
+                </div>
+                <div class="art-10 clearfix">
+                    <span class="art-picto"></span>
+                    <p>La part valorisable est extraite et le déchet ultime (non valorisable) part en centre de classe 2  pour enfouissement.</p>
+                </div>
+            </li><!-- FIN SLIDE #2-->
+            <li>
+                <h3 class="small-title">Le négoce</h3>
+                <div  class="art-1 wrapper">
+                    <h4 class="small-title">Acheter & Vendre</h4>
+                    <img src="img/hexa-aluminium.png" alt="Copeaux d'aluminium non traité" title="copeaux d'aluminium">
+
+                     <p>Le négoce est  le principe d’achat et de revente auprès de nos fournisseurs, d’industriels, de PME, d’artisans et de  particuliers. Il est question d’acheter en petites et/ou moyenne quantités et de revendre en grandes quantités les métaux ferreux et non ferreux. </p>
+                </div>
+                <div  class="art-2 wrapper">
+                    <h4 class="small-title">Traiter & Recycler</h4>
+                    <img src="img/hexa-pipe.png" alt="Tube d'aluminium" title="Tube d'aluminium">
+                     <p>Selon la catégorie, la ferraille peut être cisaillée, broyée, découpée…
+                    La valorisation des catégories de ferrailles, tournures métaux nobles, spéciaux et alliages, crasses, câbles, moteurs électriques ainsi que leur préparation  précède leur réintégration dans le circuit du recyclage .</p>
+                </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section id="clients">
+          <div class="wrapper clearfix">
+              <div  class="clearfix">
+                  <h2>Nos clients</h2>
+                  <img src="img/logo-entreprise.png" alt="Logo des entreprise ayant fait appel à SAR environnement">
+                  <p><strong></strong></p>
+                  <blockquote>
+                      <p>1000 tonnes par mois : réintégrées dans les circuits de recyclage</p>
+                      <span><b>Charlène Greiffemberg,</b><i>Responsable des commandes</i></span>
+                  </blockquote>
+
+              </div>
+
+          </div>
+      </section>
+
+
+
+<script>
+    $(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+</script>
+
+
+<script src="js/jquery.flexslider.js"></script>
+<script>
+    // Can also be used with $(document).ready()
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide"
+  });
+});
+</script>
+
+
+<!-- SCROLL EFFECT-->
+<script src="js/classie.js"></script>
+            <script src="js/cbpScroller.js"></script>
+            <script>
+                new cbpScroller( document.getElementById( 'so-scroller' ) );
+            </script>
+
+<!-- NAV effect -->
+<script type="text/javascript">
+            $(function() {
+                $(window).scroll(function(){
+                    var scrollTop = $(window).scrollTop();
+                    if(scrollTop != 0)
+                        $('#nav').stop().animate({'opacity':'0.2'},400);
+                    else
+                        $('#nav').stop().animate({'opacity':'1'},400);
+                });
+
+                $('#nav').hover(
+                    function (e) {
+                        var scrollTop = $(window).scrollTop();
+                        if(scrollTop != 0){
+                            $('#nav').stop().animate({'opacity':'1'},400);
+                        }
+                    },
+                    function (e) {
+                        var scrollTop = $(window).scrollTop();
+                        if(scrollTop != 0){
+                            $('#nav').stop().animate({'opacity':'0.2'},400);
+                        }
+                    }
+                );
+            });
+</script>
+
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+</body>
+</html>
